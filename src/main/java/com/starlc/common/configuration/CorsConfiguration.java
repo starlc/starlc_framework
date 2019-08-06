9c913509969c9f9fa7dc1d9a9aa492b51269cc85
liuc <liuc@authine.com>
2019-08-06 11:58:14 +0800

    修改

---
+++ src/main/java/com/starlc/common/configuration/CorsConfiguration.java
@@ -0,0 +1,26 @@
+package com.starlc.common.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+* @Description:    cors配置
+* @Author:         liuc
+* @CreateDate:     2019/8/6 11:41
+* @UpdateRemark:   修改内容
+* @Version:        1.0
+*/
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
